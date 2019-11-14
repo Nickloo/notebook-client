14c60c8d65733fd60eec2002835803d35ff2eb88
nickloo <--add>
2019-11-14 15:34:47 +0800

    fix

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const service = axios.create({
-    baseURL: process.env.NODE_ENV === 'production' ? "http://140.82.29.128/:7002" : "http://localhost:7002",
+    baseURL: process.env.NODE_ENV === 'production' ? "http://140.82.29.128:7002" : "http://localhost:7002",
     timeout: 5000
 })
 service.interceptors.request.use(
