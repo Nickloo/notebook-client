3a830114881236c1a6c32c40550d32ae5653baf2
nickloo <--add>
2019-11-14 10:56:06 +0800

    pref

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,13 +1,14 @@
 import axios from "axios"
 
 const service = axios.create({
-    // baseURL:"http://localhost:3000",
+    baseURL:"http://localhost:7002",
     timeout: 5000
 })
 service.interceptors.request.use(
     config => {
         config.headers['username'] = localStorage.username
         config.headers['password'] = localStorage.password
+        config.headers['token'] = localStorage.password
         return config
     }
 )
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 // vue.config.js
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'production'
-    ? '/dist/'
-    : '/',
-  devServer: {
-    proxy: 'http://localhost:7002'
-  },
+  // baseUrl: process.env.NODE_ENV === 'production'
+  //   ? '/dist/'
+  //   : '/',
+  // devServer: {
+  //   proxy: 'http://localhost:7002'
+  // },
   // pages: {
   //   index: {
   //     title: "article"
